7e334fccfc246d998447721b9ce80d894daa45ca
Psychea13 <kea260209@gmail.com>
2024-08-13 15:32:27 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+    
+# При вызове inner_function() вне test_function() будет ошибка,
+# т.к. inner_function() не находится в объемлющей зоне видимости функции test_function
